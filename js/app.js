4c4ae98fab40fd2ec7948458b7da953b16b8dab0
Michelle Leano <leanomc@hawaii.edu>
2022-03-26 12:13:17 -1000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,36 +5,33 @@
 // Find the div with the id of "name1" and replace the text n/a with "Tentacool"
 // Place your code below
 
-
+document.getElementById('name1').innerHTML = "Tentacool";
 
 // Pokemon 2
 // Find the div with the id of "type2" and replace the text n/a with "water"
 // Place your code below
 
-
-
+document.getElementById('type2').innerHTML = "water";
 
 // Pokemon 3
 // Find the div with the id of "pokeNumber3" and replace the text n/a with 116:
 // Place your code below
 
-
-
+document.getElementById('pokeNumber3').innerHTML = 116;
 
 
 // Pokemon 4
 // Find the div with the id of "bio4" and replace the text n/a with your personal description of this Pokemon.
 // Place your code below
 
-
-
+document.getElementById('bio4').innerHTML = "So very crabby";
 
 
 // Pokemon 5
 // Find the div with the id of "bio5" and replace the the font color to "red";
 // Place your code below
 
-
+document.getElementById('bio5').style.color = "red";
 
 
 
@@ -42,7 +39,7 @@
 // Find the div with the id of "name6" and replace the font size to "60px";
 // Place your code below
 
-
+document.getElementById('name6').style.fontSize = "60px";
 
 
 
@@ -50,4 +47,4 @@
 // Find the div with the id of "container" and change the background color to "green"
 // Place your code below
 
-
+document.getElementById('container').style.backgroundColor = "green";
